de264922fe213a7bb5c36e8ba209a63d31b62b65
Xianguang Yan <xyan02@calpoly.edu>
2017-05-10 21:02:01 -0700

    Finished Reflection working on Refract

--- src/pixelcolor.cpp
+++ src/pixelcolor.cpp
@@ -125,7 +125,7 @@ vec3 cook_tor(vec3 c, std::vector<Light> l, std::vector<Object *> o, float T, in
 
 float shlicksFormula(float ior, vec3 l, vec3 v) {
 	float f1 = pow(ior - 1, 2) / pow(ior + 1, 2);
-	float F = f1 + (1 - f1) * (1 - dot(l, v));
+	float F = f1 + (1 - f1) * (1 - dot(-l, v));
 	return F;
 }
 
@@ -164,16 +164,23 @@ vec3 pixelColor(vec3 dir, vec3 origin, std::vector<Light> l, std::vector<Object 
 	vec3 color = vec3(0, 0, 0);
 	float local_contribution = 1;
 	float reflection_contribution = 1;
+	float transmission_contribution = 1;
 	//vec3 pixRay = pixelray(&c, width, height, x, y, 0);
 	float T = checkHit(dir, origin, &o, width, height, x, y, mode, &index);
 	vec3 reflection_color;
+	vec3 refraction_color;
+	float sneil_ratio;
 	//Hits Something
 	if(index != -1) {
+		// Doe some basic setup for variables
+		sneil_ratio = 1.0f/o.at(index)->ior;
 		vec3 p = origin + dir * T;
 		vec3 norm = getNormal(o, index, p);
 		float fresnel_reflectance = shlicksFormula(o.at(index)->ior, dir, norm);
 		local_contribution = (1 - o.at(index)->filter) * (1 - o.at(index)->reflection);
 		reflection_contribution = (1 - o.at(index)->filter) * (o.at(index)->reflection) + (o.at(index)->filter) * (fresnel_reflectance);
+		transmission_contribution = (o.at(index)->filter) * (1 - fresnel_reflectance);
+		//Checks What method to use
 		if(brdf == 0) {
 			color = blinn_phong(origin, l, o, T, index, width, height, x, y, dir);
 			if(mode == 1) {
@@ -188,10 +195,21 @@ vec3 pixelColor(vec3 dir, vec3 origin, std::vector<Light> l, std::vector<Object 
 		}
 
 		//Check Number of Times
+		//Reflection
 		if(iterations < 6 && o.at(index)->reflection > 0) {
 			vec3 reflection_vector = dir - 2 * (dot(dir, norm)) * norm;
 			iterations++;
-			reflection_color = pixelColor(reflection_vector, (p+norm*.001f), l, o, width, height, x, y, brdf, mode, iterations) * o.at(index)->reflection;
+			reflection_color = pixelColor(reflection_vector, (p+norm*.001f), l, o, width, height, x, y, brdf, mode, iterations);
+		}
+		//Refraction
+		if(iterations < 6 && o.at(index)->refraction > 0) {
+			if(dot(dir, norm) > 0) {
+				norm = -norm;
+				sneil_ratio = o.at(index)->ior;
+			}
+			vec3 refraction_vector = (sneil_ratio) * (dir - dot(dir, norm) * norm) - norm * sqrt(1 - pow(sneil_ratio,2) * (1 - pow(dot(dir, norm), 2)));	
+			iterations++;
+			refraction_color = pixelColor(refraction_vector, (p-norm *.001f), l, o, width, height, x, y, brdf, mode, iterations);
 		}
 	}
 	if(mode == 1) {
@@ -199,7 +217,7 @@ vec3 pixelColor(vec3 dir, vec3 origin, std::vector<Light> l, std::vector<Object 
 		cout << "Color: (" << (unsigned int) round(255.f * color.r) << ", " << (unsigned int) round(255.f * color.g) << ", " << (unsigned int) round(255.f * color.b) << ")" << endl;
 	}
 	
-	return local_contribution * color + reflection_color * reflection_contribution;
+	return local_contribution * color + reflection_color * reflection_contribution + refraction_color * transmission_contribution;
 }
 
 
